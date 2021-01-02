67f77b0cd3e0d76f0b6c55297bac62a33b3cbaaa
Saurabh Zarapkar <sazarapkar@gmail.com>
2021-01-02 17:26:54 -0500

    cleaned up js file

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 
+//navigation Animation
+function navAnimation(dir1, dir2) {
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${dir1}-${i+1}`, `slide-${dir2}-${i+1}`);
+    });
+}
 
 function toggleNav() {
     //Toggle Menu bars open/close
@@ -16,36 +23,16 @@ function toggleNav() {
         //Animate in overlay
         overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
         //Animate in Navi-items
-        nav1.classList.remove('slide-out-1');
-        nav1.classList.add('slide-in-1');
-        nav2.classList.remove('slide-out-2');
-        nav2.classList.add('slide-in-2');
-        nav3.classList.remove('slide-out-3');
-        nav3.classList.add('slide-in-3');
-        nav4.classList.remove('slide-out-4');
-        nav4.classList.add('slide-in-4');
-        nav5.classList.remove('slide-out-5');
-        nav5.classList.add('slide-in-5');
+        navAnimation('out', 'in');
     } else {
         //Animate out overlay
         overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
         //Animate out Navi-items
-        nav1.classList.remove('slide-in-1');
-        nav1.classList.add('slide-out-1');
-        nav2.classList.remove('slide-in-2');
-        nav2.classList.add('slide-out-2');
-        nav3.classList.remove('slide-in-3');
-        nav3.classList.add('slide-out-3');
-        nav4.classList.remove('slide-in-4');
-        nav4.classList.add('slide-out-4');
-        nav5.classList.remove('slide-in-5');
-        nav5.classList.add('slide-out-5');
+        navAnimation('in', 'out');
     }
 }
 //Event Listeners
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav)
-nav2.addEventListener('click', toggleNav)
-nav3.addEventListener('click', toggleNav)
-nav4.addEventListener('click', toggleNav)
-nav5.addEventListener('click', toggleNav)
+navItems.forEach((nav) => {
+    nav.addEventListener('click', toggleNav);
+});
